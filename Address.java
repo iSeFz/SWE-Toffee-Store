2ca53ea6484ca0a6bbc24ddba207ac12896479fb
iSeFz <seifyahia@gmail.com>
2023-05-07 23:06:12 +0300

    Added Address and GeneralUser classes implementation

---
+++ Address.java
@@ -0,0 +1,9 @@
+public class Address {
+    private String governorate;
+    private String district;
+    private String street;
+    private int buildingNumber;
+    private int floorNumber;
+    private int flatNumber;
+    private String landmark;
+}
---
+++ GeneralUser.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class GeneralUser{
+    private ArrayList<OrderedItem> cart;
+    // Add an item to cart
+    public void addToCart(Item item, float quantity, boolean type){
+        cart.add(item.orderItem(quantity, type));
+    }
+    // View cart items
+    public void viewCartItems(){
+        for(OrderedItem cartItem : cart){
+            System.out.println("Name: " + cartItem.item.getName());
+            System.out.println("Quantity: " + cartItem.quantity);
+            System.out.println("Price: " + cartItem.calcPrice());
+        }
+    }
+}
