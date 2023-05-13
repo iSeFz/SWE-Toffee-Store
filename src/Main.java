b7d0db9ed25890f8e0bff4d4b5ae3ea7fb8bef35
iSeFz <seifyahia@gmail.com>
2023-05-13 16:22:21 +0300

    Modified the main menu & added the display catalog option

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static AccountsManager accManager = new AccountsManager();
     public static ShoppingManager shopManager = new ShoppingManager();
-    public static void readFile() throws IOException {
-        accountParse("data/RegisteredUsers.txt");
-        itemParse("data/Items.txt");
-    }
+
+    // Import accounts from RegisteredUsers.txt into the accManager
     public static void accountParse(String path) throws FileNotFoundException, FileNotFoundException {
         String userName, email, password;
         File file = new File(path);
@@ -24,6 +22,7 @@ public class Main {
         }
     }
 
+    // Import items from Items.txt into the shopManager
     public static void itemParse(String path) throws IOException {
         String name, cat, description, brand, imageURL, status;
         float price, availableKG;
@@ -41,11 +40,14 @@ public class Main {
             availableKG = scf.nextFloat();
             availableUnits = scf.nextInt();
             status = scf.next();
+            status = status.substring(0,status.length() - 1);
             shopManager.addItem(name, cat, description, imageURL, brand, price, availableKG, availableUnits);
         }
     }
+
     public static void main(String[] args) throws IOException {
-        readFile();
+        accountParse("data/RegisteredUsers.txt");
+        itemParse("data/Items.txt");
         Scanner choice = new Scanner(System.in);
         int option;
         System.out.println("\tWelcome to our Toffee Shop!");
@@ -53,28 +55,26 @@ public class Main {
             System.out.println("\nOur program offers the following features:\n" +
                     "1. Register a new account\n" +
                     "2. Login to your account\n" +
-                    "3. Display Catalog of Toffees\n" +
-                    "4. Shop for Toffees\n" +
-                    "5. Make an order to be paid through COD (Cash on delivery)\n" +
-                    "6. Exit");
+                    "3. Display Catalog of Items\n" +
+                    "4. Shop for Toffees & Add to Cart\n" +
+                    "5. View Cart & Checkout\n" +
+                    "6. Make an order to be paid through COD (Cash on delivery)\n" +
+                    "7. Exit");
             System.out.print("Choose one of the above options >> ");
             option = choice.nextInt();
-            // Register a new account
             if (option == 1)
                 accManager.signUp();
-            // Login to your account
             else if (option == 2)
                 accManager.login();
-            // Display Catalog of Toffees
             else if (option == 3)
-                System.out.println("\n\tDisplay Catalog of Toffees!");
-            // Shop for Toffees & add them to cart
+                shopManager.displayCatalog();
             else if (option == 4)
-                System.out.println("\n\tShop for Toffees!");
-            // Make an order to be paid through cash upon delivery
+                System.out.println("\n\tShop for Toffees & Add to Cart!");
             else if (option == 5)
-                System.out.println("\n\tMake an order to be paid through cash upon delivery!");
+                System.out.println("\n\tView Cart & Checkout!");
             else if (option == 6)
+                System.out.println("\n\tMake an order to be paid through cash upon delivery!");
+            else if (option == 7)
                 break;
             else
                 System.out.println("\n\tINVALID OPTION! Enter only numbers from 1 to 6");
